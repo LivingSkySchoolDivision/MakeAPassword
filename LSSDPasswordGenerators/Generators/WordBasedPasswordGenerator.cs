defcfebf298b3d3e6bc05bef5090ab99129dd22c
Mark Strendin <mark.strendin@lskysd.ca>
2020-03-11 10:09:15 -0600

    Don't allow a password without a special character (part 2)

--- LSSDPasswordGenerators/Generators/WordBasedPasswordGenerator.cs
+++ LSSDPasswordGenerators/Generators/WordBasedPasswordGenerator.cs
@@ -10,7 +10,7 @@ namespace LSSDPasswordGenerators.Generators
     public class WordBasedPasswordGenerator
     {
         private readonly Random _random = new Random(Guid.NewGuid().GetHashCode());
-        private readonly List<string> _separators = new List<string>() { "#", "&", ";", "!", ":", "_", "-", "?", "?!", "*", "#", "+", ".", ",", ",SoI", ",And", ",The", ",WithThe", ",With", ",without", ",But", ",ButWith", ",ButAlso", ",InsteadOf", ",Then", ",ThenA", ",AlongA", ",Because", ",AlongWith", ",AlongThe", ",OverThe", ",OverA", ",BecauseOf", ",BecauseOfThe", "More-Than", "Less-Than", ",Also", ",UntilThe", ",Plus", ",At", ",AtThe", "InA", ",AlongSideA", ",BesideThe" };
+        private readonly List<string> _separators = new List<string>() { "#", "&", ";", "!", ":", "_", "-", "--", "!!", "?", "?!", "*", "#", "+", ".", ",", ",SoI", ",And", ",The", ",WithThe", ",With", ",without", ",But", ",ButWith", ",ButAlso", ",InsteadOf", ",Then", ",ThenA", ",AlongA", ",Because", ",AlongWith", ",AlongThe", ",OverThe", ",OverA", ",BecauseOf", ",BecauseOfThe", "More-Than", "Less-Than", ",Also", ",UntilThe", ",Plus", ",At", ",AtThe", ",AlongSideA", ",BesideThe" };
 
         private readonly List<string> _wordlistMedium;
         private readonly List<string> _wordlistHigh;
