dd6952501a27df834c011e6848a22da8be75dac8
Mark Strendin <mark.strendin@lskysd.ca>
2020-09-29 15:02:24 -0600

    Add the numeric PIN generator missing from the last commit

---
+++ LSSDPasswordGenerators/Generators/NumericalPINGenerator.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LSSDPasswordGenerators.Generators
+{
+    public class NumericalPINGenerator
+    {
+        private readonly Random _random = new Random(Guid.NewGuid().GetHashCode());
+        private string allowedCharacters = "9876543210";        
+
+        public string GeneratePassword(int length)
+        {
+            StringBuilder password = new StringBuilder();
+
+            for (int x = 0; x < length; x++)
+            {
+                password.Append(allowedCharacters[_random.Next(0, allowedCharacters.Length)]);
+            }
+
+            return password.ToString();
+        }
+        
+    }
+}
