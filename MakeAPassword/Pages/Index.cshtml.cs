841034ba91a6fe5806aa0de5c32a16b5fb7ae2de
Mark Strendin <mark.strendin@gmail.com>
2022-04-19 21:58:22 -0600

    Make the homepage work

--- MakeAPassword/Pages/Index.cshtml.cs
+++ MakeAPassword/Pages/Index.cshtml.cs
@@ -7,9 +7,14 @@ public class IndexModel : PageModel
 {
     private readonly ILogger<IndexModel> _logger;
 
-    public IndexModel(ILogger<IndexModel> logger)
+    public readonly ComplexPasswordService ComplexPasswordGenerator;
+    public readonly WordPasswordService WordPasswordGenerator;
+
+    public IndexModel(ILogger<IndexModel> logger, ComplexPasswordService complexService, WordPasswordService wordService)
     {
         _logger = logger;
+        this.ComplexPasswordGenerator = complexService;
+        this.WordPasswordGenerator = wordService;
     }
 
     public void OnGet()
